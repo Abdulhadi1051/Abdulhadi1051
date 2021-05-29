83f8bde117cafcffec79c484001726b765312696
Abdulhadi1051 <81959131+Abdulhadi1051@users.noreply.github.com>
2021-05-29 11:10:26 +0100

    Add files via upload

---
+++ hadi droid.py
@@ -0,0 +1,91 @@
+USSD_CODE = ('UNITED BANK for Africa : *919#')
+print (USSD_CODE)
+step1='please type in your ussd code'
+print (step1)
+step2='you have enter the code for uba'
+print(step2)
+option=['1. Airtime-Self','2. Airtime-Other','3.Transfer-UBA','4.Transfer Other Bank','5. Tranfer UBA prepaid','6.Check balance','7. Pay Bills','8.Next']
+menu='00.menu'
+print(option[0])
+opt=('please enter your pin',
+'0. menu')
+print(opt)
+optA=('please enter amount(50:5000)')
+print(optA)
+amtA=40
+print('transaction sucessful you will recieve  an sms soon')
+end='00.Back','0.Menu'
+print(end)
+print(option)
+print(option[1])
+print(opt)
+rec=('please enter the phone number of beneficiary')
+print(rec)
+optB=('please enter amount(50:5000)')
+print(optB)
+amtB=200
+print('transaction succesful')
+end='00.Back','0.Menu'
+print(end)
+print(menu)
+print(option)
+print(option[2])
+print(opt)
+py=('please enter UBA account number or name of beneficiary')
+print(py)
+account2=2137541959
+print(account2)
+amtC=300
+print('Do you want to send money to',account2)
+select=('1.Yes','2.No')
+print(select)
+yes=1
+print(yes)
+print('Do you want to save this beneficiary')
+print('1.Yes','2.No')
+print(yes)
+result='transaction is been proceed:'
+print(result)
+end='0.Back :','00. menu'
+print(end)
+print(option)
+print(option[3])
+opt3='please enter your pin:','0. Menu'
+print(opt3)
+py=('please the acount or name of the beneficiary ')
+print(py)
+account3=31224617
+print(account3)
+banks3=['access bank','first bank','union bank']
+print(banks3[1])
+print('Do you want to send money to:',account3)
+select=('1. Yes:','2. No')
+print(select)
+print(yes)
+print('Do you want to save this benificiary ?')
+print(select)
+print(yes)
+print(result)
+print(end)
+print(menu)
+print(option)
+print(option[4])
+print(opt)
+print('please enter client id number (10 digits) or name of beneficiary')
+client_no=9231648234
+print(client_no,'those not exist')
+print(end)
+menu='00. menu'
+print(menu)
+print(option)
+print(option[5])
+print(opt)
+print('Do you want to proceed 10 fee apply:', 'Do you wish to continue:' ,select)
+print(yes)
+print('Txn account balance')
+print('ac; 21xxxxxxx7x')
+print('Amt;900')
+print('Des;account/balance/23456779/533921/ umidigi A7 pro')
+print('Date;28/5/2021')
+print('balance;900')
+print('Download bit.ly.newUBapp')
